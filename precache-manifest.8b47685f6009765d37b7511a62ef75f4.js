24103844d1f577d267133186640a09ad612c4f28
darren <darrenwlsn54@gmail.com>
2018-10-31 12:56:41 +1000

    Updates

---
+++ precache-manifest.8b47685f6009765d37b7511a62ef75f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2813c824fb6d84b08be",
+    "url": "/darrenwlsn/contactmanager/static/js/runtime~main.e2813c82.js"
+  },
+  {
+    "revision": "b89ae9f60d1280712f97",
+    "url": "/darrenwlsn/contactmanager/static/js/main.b89ae9f6.chunk.js"
+  },
+  {
+    "revision": "257ab808f40b1d03c540",
+    "url": "/darrenwlsn/contactmanager/static/js/1.257ab808.chunk.js"
+  },
+  {
+    "revision": "b89ae9f60d1280712f97",
+    "url": "/darrenwlsn/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "257ab808f40b1d03c540",
+    "url": "/darrenwlsn/contactmanager/static/css/1.bfb7d3cd.chunk.css"
+  },
+  {
+    "revision": "727fbda8c7ad448bdcc15ced56656c3c",
+    "url": "/darrenwlsn/contactmanager/index.html"
+  }
+];
